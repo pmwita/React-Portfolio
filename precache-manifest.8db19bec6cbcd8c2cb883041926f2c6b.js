69a17ab03bd56ae84dd736548ef28d208625febb
Peter Mwita <pmwita2017@gmail.com>
2022-12-11 14:03:47 +0300

    Updates

---
+++ precache-manifest.8db19bec6cbcd8c2cb883041926f2c6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbc5a6d413f50fc58731",
+    "url": "/React-Portfolio/static/css/main.47dbb278.chunk.css"
+  },
+  {
+    "revision": "bbc5a6d413f50fc58731",
+    "url": "/React-Portfolio/static/js/main.bbc5a6d4.chunk.js"
+  },
+  {
+    "revision": "fe945e02906a73ec196a",
+    "url": "/React-Portfolio/static/js/1.fe945e02.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "56f18e5a3420271069d0f5af60f13253",
+    "url": "/React-Portfolio/index.html"
+  }
+];
